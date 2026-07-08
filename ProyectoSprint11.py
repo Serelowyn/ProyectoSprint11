284cab732c01f7d214dc8f0315c93845796ad392
Memo <fretn655@gmail.com>
2026-07-08 13:13:48 -0600

    se carga la libreria pandas, seguido de eso se abre el dataset que se usara churn.csv

---
+++ ProyectoSprint11.py
@@ -0,0 +1,13 @@
+# ------------------ Importaciones
+
+import pandas as pd
+
+# ------------------ Importaciones
+
+
+# 1. Descarga y prepara los datos.  Explica el procedimiento.
+
+# data = pd.read_csv("/datasets/churn.csv") para tripleten
+data = pd.read_csv(r"churn.csv")
+
+"""se descarga el archivo desde la plataforma (churns.csv) y se carga con el nombre de variable data"""
